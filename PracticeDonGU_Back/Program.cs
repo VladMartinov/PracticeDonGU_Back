0bad5060ea5a93b1c58886599b154338f42a063a
Vlad Martynov <vladmartinov1704@mail.ru>
2024-06-14 10:24:14 +0300

    add: cors configuration.

--- PracticeDonGU_Back/Program.cs
+++ PracticeDonGU_Back/Program.cs
@@ -14,6 +14,17 @@ services.AddDbContext(configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    // Vue
+    options.AddPolicy("DefaultPolicy", policyBuilder =>
+    {
+        policyBuilder.WithOrigins(builder.Configuration.GetSection("Origin").Value)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("DefaultPolicy");
 
 app.UseAuthorization();
 
